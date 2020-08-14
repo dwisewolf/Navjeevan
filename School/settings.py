9d758c21406041fa796209208c31b892a4d8f65f
surajwisewolf <suraj@wisewolf.tech>
2020-08-14 11:00:32 +0530

    added ip

--- School/settings.py
+++ School/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p_oiabfi_1(#fyrk7g#8j#-tt+oyjtny*ih)$m2@$q0v0qr4c$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['139.59.79.78', '127.0.0.1']
+ALLOWED_HOSTS = ['165.232.76.212', '127.0.0.1']
 
 
 # Application definition
@@ -164,7 +164,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -181,6 +181,7 @@ LOGIN_REDIRECT_URL = '/home/usersList/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIR = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
