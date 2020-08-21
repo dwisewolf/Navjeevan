b9dd0a8330416f70e162cc8fe2dd0e3cb08efc48
dwisewolf <dev.wisewolf@gmail.com>
2020-08-21 09:55:46 +0000

    Commit

--- vimeoapi/models.py
+++ vimeoapi/models.py
@@ -5,12 +5,12 @@ import jsonfield
 # Create your models here.
 
 class Vimeo(models.Model):
-    title = models.CharField(max_length=100)
-    college = models.CharField(max_length=10)
-    sub = models.CharField(max_length=10)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    college = models.CharField(max_length=100, null=True, blank=True)
+    sub = models.CharField(max_length=100, null=True, blank=True)
     data = jsonfield.JSONField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -19,5 +19,5 @@ class Vimeo(models.Model):
 class VimeoStatus(models.Model):
     status = models.BooleanField(default=False)
     count = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
